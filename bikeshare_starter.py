bb91d816d6fd817203e2c45a9a97244c58ecc58f
Thomas Werner (T9-1) <thomas.werner@cariad.technology>
2026-02-20 11:13:00 +0100

    refactor: Improve  naming of load_data return value

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -38,11 +38,11 @@ def load_data(city, month, day):
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     Returns:
-        df - Pandas DataFrame containing city data filtered by month and day
+        filteredData - Pandas DataFrame containing city data filtered by month and day
     """
 
 
-    return df
+    return filteredData
 
 
 def time_stats(df):
